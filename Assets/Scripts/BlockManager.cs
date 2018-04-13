c376f2a45fc6feff163a53faca9e3cde783a0d90
Kim HyunWoo <stories2@naver.com>
2018-04-13 19:49:40 +0900

    BlockManager : moving block

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+	static readonly float BLOCK_MOVE_SPEED = 8.0f,
+		OUT_OF_SCREEN_X = -10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveCurrentObj (BLOCK_MOVE_SPEED);
+		DestroyWhenObjectOutOfScreen ();
+	}
+
+	void MoveCurrentObj(float speed) {
+		transform.Translate (Vector3.left * Time.deltaTime * speed);
+	}
+
+	void DestroyWhenObjectOutOfScreen() {
+		Vector3 pos = gameObject.transform.position;
+		if (pos.x < OUT_OF_SCREEN_X) {
+			Destroy (gameObject);
+		}
+	}
+}
